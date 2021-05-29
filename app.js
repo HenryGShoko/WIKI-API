87f3624f7193b955ad0e853050ce63ce078a1d38
HenryGShoko <gshokx91@gmail.com>
2021-05-29 12:01:22 +0200

    GET All Articles

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
-mongoose.connect('mongodb://localhost:27017/wikiDB', {userNewUrlParser: true})
+mongoose.connect('mongodb://localhost:27017/wikiDB', {useNewUrlParser: true})
 
 const articleSchema = {
     title: String,
@@ -21,7 +21,15 @@ const articleSchema = {
 
 const Article = mongoose.model('Article',articleSchema)
 
-//TODO
+app.get('/articles', (req, res) => {
+Article.find((err, foundArticles) =>{
+    if(!err){
+        res.send(foundArticles)
+    }else{
+        res.send(err)
+    }
+})
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
